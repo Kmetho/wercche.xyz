4d8acf7d1f946948f4546496bda97e56ae314cf0
Kmetho <94000429+Kmetho@users.noreply.github.com>
2023-03-19 00:04:38 +0100

    Give up for today

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
 const cursorB = document.querySelector(".big");
 const cursorS = document.querySelector(".small");
+const anchors = document.querySelectorAll("a");
+const innerBig = document.querySelector(".inner-big");
+const innerBigger = document.querySelector(".inner-bigger");
+const innerTxt = document.querySelector(".inner-txt");
 
 document.addEventListener("mousemove", (e) => {
   cursorB.setAttribute(
@@ -10,4 +14,15 @@ document.addEventListener("mousemove", (e) => {
     "style",
     "top: " + e.pageY + "px; left: " + e.pageX + "px;"
   );
+});
+
+anchors.addEventListener(onmouseenter, () => {
+  innerBig.setAttribute("style", "visibility: hidden;");
+  innerBigger.setAttribute("style", "visibility: visible;");
+  innerTxt.setAttribute("style", "visibility: visible;");
+});
+anchors.addEventListener(onmouseleave, () => {
+  innerBig.setAttribute("style", "visibility: visible;");
+  innerBigger.setAttribute("style", "visibility: hidden;");
+  innerTxt.setAttribute("style", "visibility: hidden;");
 });
