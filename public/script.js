822de614c859f50f5a122481210332415d0f92ae
Kmetho <94000429+Kmetho@users.noreply.github.com>
2023-03-30 22:34:20 +0200

    Update

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ const cursorS = document.querySelector(".small");
 document.addEventListener("mousemove", (e) => {
   cursorB.setAttribute(
     "style",
-    "top: " + e.pageY + "px; left: " + e.pageX + "px;"
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
   );
   cursorS.setAttribute(
     "style",
