7f5d6a120a10e82d8f340b1d4633b6bfbaaf8b3b
Kmetho <94000429+Kmetho@users.noreply.github.com>
2023-04-20 14:47:27 +0200

    UPDATE

--- public/sketch.js
+++ public/sketch.js
@@ -1,30 +1,23 @@
 let font;
 let anim;
 
-
-function preload() {
-  font = loadFont("./assets/fonts/Isobare-Regular.otf");
-}
-
 function setup() {
-  createCanvas(window.innerWidth / 2, window.innerHeight);
-  anim = createGraphics(window.innerWidth / 2, window.innerHeight);
+  createCanvas(window.innerWidth, window.innerHeight);
+  background(255);
 }
 
 function draw() {
-  background(0);
-  noFill();
-  rect(0, 0, window.innerWidth / 2, window.innerHeight);
-
-  anim.background(0);
-  anim.fill(255);
-  anim.textFont(font);
-  anim.textSize(50);
-  anim.push();
-  anim.translate(width / 4, height);
-  anim.textAlign(CENTER, CENTER);
-  anim.text("wercche.xyz", 0, 0);
-  anim.pop();
-
-  image(anim, 0, 0);
+  ellipse(mouseX, mouseY, 20, 20);
 }
+
+// anim.background(0);
+// anim.fill(255);
+// anim.textFont(font);
+// anim.textSize(50);
+// anim.push();
+// anim.translate(width / 4, height);
+// anim.textAlign(CENTER, CENTER);
+// anim.text("wercche.xyz", 0, 0);
+// anim.pop();
+
+// image(anim, 0, 0);
